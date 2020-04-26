403eb16c59921b1908d12a777a636673a076e639
maxsust <maxsust@mail.ru>
2020-04-26 23:11:00 +0400

    фыв
    фыв

---
+++ config.php
@@ -0,0 +1,33 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://volody7s.beget.tech/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://volody7s.beget.tech/');
+
+// DIR
+define('DIR_APPLICATION', '/home/v/volody7s/volody7s.beget.tech/public_html/catalog/');
+define('DIR_SYSTEM', '/home/v/volody7s/volody7s.beget.tech/public_html/system/');
+define('DIR_IMAGE', '/home/v/volody7s/volody7s.beget.tech/public_html/image/');
+define('DIR_STORAGE', DIR_SYSTEM . 'storage/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/theme/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'volody7s_db');
+define('DB_PASSWORD', '5gAZ7uCd');
+define('DB_DATABASE', 'volody7s_db');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
+
+//V3TOYS
+define('AFF_KEY', 'vph');
